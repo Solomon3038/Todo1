0a72f8313c88dd0a190451120faeccddf0ef2891
solomon <anhalt79@gmail.com>
2018-07-15 21:52:26 +0200

    -

--- src/main/java/com/solomon/todo/intergration/telegram/TelegramWebhookInitializer.java
+++ src/main/java/com/solomon/todo/intergration/telegram/TelegramWebhookInitializer.java
@@ -4,12 +4,10 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SetWebhook;
 import com.pengrad.telegrambot.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -18,25 +16,26 @@ import java.net.URI;
 @Slf4j
 public class TelegramWebhookInitializer implements ApplicationListener<ServletWebServerInitializedEvent> {
 
-    @Autowired
-    private TelegramBot bot;
+    private final TelegramBot bot;
 
-    private WebApplicationContext ctx;
+    private final URI webhookHost;
 
-    @Value("${telegram.webhook.host}")
-    private URI webhookHost;
+    private final String path;
 
-    @Value("${telegram.webhook.path}")
-    private String path;
-
-    public void setWebhook() {
-
-
+    public TelegramWebhookInitializer(
+            TelegramBot bot,
+            @Value("${telegram.webhook.host}") URI webhookHost,
+            @Value("${telegram.webhook.path}") String path
+    ) {
+        this.bot = bot;
+        this.webhookHost = webhookHost;
+        this.path = path.startsWith("/") ? path.substring(1) : path;
     }
+
 
     @Override
     public void onApplicationEvent(ServletWebServerInitializedEvent event) {
-        String  uri = UriComponentsBuilder.fromUri(webhookHost).pathSegment(path).build().toString();
+        String uri = UriComponentsBuilder.fromUri(webhookHost).pathSegment(path).build().toString();
         SetWebhook request = new SetWebhook().url(uri);
         BaseResponse response = bot.execute(request);
         log.info("URL: {}, Response {}", uri, response);
